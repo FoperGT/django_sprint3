6f1139c1f9b95ab6f636ea871bdfe2cae1b4457a
fopergt <fopergt@gmail.com>
2025-05-01 17:39:12 +1000

    [WIP]

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -5,17 +5,17 @@ admin.site.register(Category)
 admin.site.register(Location)
 admin.site.register(Post)
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name')
+#     search_fields = ('name',)
 
 
-class LocationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
+# class LocationAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name')
+#     search_fields = ('name',)
 
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'category', 'pub_date', 'is_published')
-    search_fields = ('title', 'author__username', 'category__name')
-    list_filter = ('category', 'is_published', 'pub_date')
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title', 'author', 'category', 'pub_date', 'is_published')
+#     search_fields = ('title', 'author__username', 'category__name')
+#     list_filter = ('category', 'is_published', 'pub_date')
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,25 +1,51 @@
-from django.shortcuts import render
-from .models import Post
-
-
-def index(request):
-    posts = Post.objects.all().order_by('-pub_date')
-
-    return render(request, "blog/index.html", {"posts": posts})
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from django.utils.timezone import now
+from .models import Post, Category
 
 
 def post_detail(request, pk):
-    post = Post.objects.get(id=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', {'post': post})
 
-    return render(request, "blog/detail.html", {"post": post})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 10
+    ordering = 'id'
+
+    def get_queryset(self):
+        return Post.objects.filter(
+            category__is_published=True,
+            is_published=True,
+            pub_date__lte=now()
+        ).order_by(self.ordering)[:5]
 
 
 def category_posts(request, category_slug):
-    posts = Post.objects.filter(category=category_slug)
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True)
+    posts = Post.objects.filter(
+        category=category,
+        is_published=True,
+        created_at__lte=now(),
+        pub_date__lte=now())
+    context = {'category': category, 'post_list': posts}
+    return render(request, 'blog/category.html', context)
 
-    return render(
-        request,
-        "blog/category.html",
-        {"category_slug": category_slug,
-         "posts": posts}
-    )
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get_object(self):
+        return get_object_or_404(Post.objects.filter(
+            category__is_published=True,
+            is_published=True,
+            pub_date__lte=now()
+        ), pk=self.kwargs['pk'])
